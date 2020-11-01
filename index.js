a604f812ce167bf5fea2c136dc3c1130e5b512ea
DharmarajX24 <63334359+DharmarajX24@users.noreply.github.com>
2020-11-01 20:42:32 +0530

    Added Process IDs

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-const process = {};
+const processes = {};
 
-exports.start = () => {
-  process["started"] = Date.now();
-  return true;
+exports.start = (name) => {
+  const processId = name || genId();
+  processes[processId] = { started: Date.now() };
+  return { result: true, pid: processId };
 };
- 
-exports.stop = () => {
-  process["completed"] = Date.now();
-  if (process["started"]) { 
-    return String(process["completed"] - process["started"]) + " ms"
+
+exports.stop = (name) => {
+  if (name) {
+    if (processes[name] && processes[name]["started"]) {
+      processes[name]["completed"] = Date.now();
+      return {
+        result: true,
+        data:
+          name +
+          ": " +
+          String(processes[name]["completed"] - processes[name]["started"]) +
+          " ms",
+      };
+    } else {
+      return {
+        result: false,
+        data:
+          "No process running. Be sure to start a process by using [exeTime.start()]",
+      };
+    }
   } else {
-    return "No process running. Be sure to start a process by using [exeTime.start()]"
+    return {
+      result: false,
+      data: "Please provide ID/Name of the process to stop.",
+    };
   }
 };
 
-// function genId() {
-//   const alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJJKLMNOPQRSTUVWXYZ";
-//   const numbers = "0123456789";
-//   const char = (alphabets + numbers).split("");
-//   const id = [];
-//   for (let i = 0; i < 8; i++) {
-//     id.push(char[Math.floor(Math.random() * (char.length - 1))]);
-//   }
-//   return id.join("");
-// }
+function genId() {
+  return rn() + ra() + ra() + rn();
+}
+
+function rn() {
+  return String(Math.floor(Math.random() * 8999) + 1000);
+}
+
+function ra() {
+  const char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  let gra = [];
+  for (let i = 0; i < 4; i++) {
+    gra.push(char[Math.floor(Math.random() * (char.length - 1))]);
+  }
+  return gra.join("");
+}
